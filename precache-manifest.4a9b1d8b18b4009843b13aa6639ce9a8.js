dbfc898c6bcca66f95666799b81e1996d47caa67
when-i-code <adrianasavastre@gmail.com>
2020-02-17 20:58:37 +0200

    Updates

---
+++ precache-manifest.4a9b1d8b18b4009843b13aa6639ce9a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419f63975ce4a8ed9448fcfe4bd3e0e4",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "4a98a72056e3ad3a709c",
+    "url": "/quiz/static/css/main.0cdf66a2.chunk.css"
+  },
+  {
+    "revision": "14609fbf67c66c0ea51b",
+    "url": "/quiz/static/js/2.5bbeec7a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quiz/static/js/2.5bbeec7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a98a72056e3ad3a709c",
+    "url": "/quiz/static/js/main.2e4ab3b5.chunk.js"
+  },
+  {
+    "revision": "c7bedca3bd359de5a688",
+    "url": "/quiz/static/js/runtime-main.d08f2342.js"
+  }
+]);
